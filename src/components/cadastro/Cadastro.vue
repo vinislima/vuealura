122733fc814a24e1d9487f49e16d29cd189b8a88
vinislima <vinislima@gmail.com>
2022-02-16 10:48:09 -0300

    estudo

--- src/components/cadastro/Cadastro.vue
+++ src/components/cadastro/Cadastro.vue
@@ -11,12 +11,14 @@
 
       <div class="controle">
         <label for="titulo">TÍTULO</label>
-        <input id="titulo" autocomplete="off" v-model.lazy="foto.titulo">
+        <input data-vv-as="título" name="titulo" v-validate data-vv-rules="required|min:4|max:30" id="titulo" autocomplete="off" v-model="foto.titulo">
+        <span class="erro" v-show="errors.has('titulo')">{{ errors.first('titulo') }}</span>
       </div>
 
       <div class="controle">
         <label for="url">URL</label>
-        <input id="url" autocomplete="off" v-model.lazy="foto.url">
+        <input name="url" v-validate data-vv-rules="required" id="url" autocomplete="off" v-model="foto.url">
+        <span class="erro" v-show="errors.has('url')">{{ errors.first('url') }}</span>
         <imagem-responsiva v-show="foto.url" :url="foto.url" :titulo="foto.titulo"/>
       </div>
 
@@ -63,15 +65,20 @@ export default {
 
       grava() {
 
-          console.log(this.foto);
+          this.$validator
+            .validateAll()
+            .then(success => {
+              if(success) {
 
-          this.service
-            .cadastra(this.foto)
-            .then(() => {
-              if(this.id) this.$router.push({ name : 'home' });
-              this.foto = new Foto()
-            },
-            err => console.log(err));
+                this.service
+                    .cadastra(this.foto)
+                    .then(() => {
+                        if(this.id) this.$router.push({ name : 'home' });
+                        this.foto = new Foto()
+                      },
+                      err => console.log(err));
+              }
+            });
       }
   },
 
@@ -113,5 +120,9 @@ export default {
 
   .centralizado {
     text-align: center;
+  }
+
+  .erro {
+    color: red;
   }
 </style>
