cfba03b2529798b45b510b01f8ca79ba7ff1935a
vinislima <vinislima@gmail.com>
2022-02-17 09:16:02 -0300

    estudo

--- src/components/shared/botao/Botao.vue
+++ src/components/shared/botao/Botao.vue
@@ -9,14 +9,14 @@ export default {
     props: {
 
         tipo: {
-            required: true, 
+            required: true,
             type: String
         },
 
         rotulo: {
-            required: true, 
+            required: true,
             type: String
-        }, 
+        },
 
         confirmacao: Boolean,
         estilo: String
@@ -49,7 +49,8 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+
     .botao {
         display: inline-block;
         padding: 10px;
--- src/main.js
+++ src/main.js
@@ -5,12 +5,22 @@ import VueRouter from 'vue-router';
 import { routes } from './routes';
 import './directives/Transform';
 import VeeValidate from 'vee-validate';
+import msg from './pt_BR.js';
+import 'bootstrap/dist/css/bootstrap.css';
+import './assets/css/teste.css';
 
 
 Vue.use(VueResource);
 Vue.http.options.root = 'http://localhost:3000';
 Vue.use(VueRouter);
-Vue.use(VeeValidate);
+Vue.use(VeeValidate, {
+  locale: 'pt_BR',
+  dictionary: {
+    pt_BR: {
+      messages: msg
+    }
+  }
+});
 
 const router = new VueRouter({
   routes,
